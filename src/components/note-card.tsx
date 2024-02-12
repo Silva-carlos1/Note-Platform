929b14b1b771b231e1d2ab7707160e387118c613
Carlos <silva.carloseduardo.ds@gmail.com>
2024-02-12 11:23:33 -0300

    feat: Use Radix library for initial modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,18 +1,39 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
 
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab sint aut
-        sequi esse ipsam. Illum totam rem, ex, ipsum maxime sit quaerat quas
-        sunt architecto minima voluptas, nostrum quasi. Quibusdam. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Fugit dicta sequi hic non
-        est placeat, architecto minus, qui deleniti sunt dignissimos officia in,
-        provident optio eum excepturi quam ipsa amet.
-      </p>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.DialogPortal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[648px] w-full h-[60vh] bg-slate-700 rounded-md">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+          </div>
+        </Dialog.Content>
+      </Dialog.DialogPortal>
+    </Dialog.Root>
   );
 }

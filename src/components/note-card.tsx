0da243bf3d17b773ee041e173b49991ca10a6231
Carlos <silva.carloseduardo.ds@gmail.com>
2024-02-13 13:48:28 -0300

    chore: Use Radix library for initial modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface NoteCardProps {
   note: {
@@ -22,16 +24,25 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.DialogPortal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[648px] w-full h-[60vh] bg-slate-700 rounded-md">
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+
           <div className="flex flex-1 flex-col gap-3 p-5">
-          <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
-        </span>
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
 
-        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
-
-        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja <span className="text-red-400 group-hover:underline">Apagar essa nota?</span>
+          </button>
         </Dialog.Content>
       </Dialog.DialogPortal>
     </Dialog.Root>

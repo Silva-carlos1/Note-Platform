b72127ac4709d8cee02eb909be5dd69dbd8074fa
Carlos <silva.carloseduardo.ds@gmail.com>
2024-02-16 14:43:14 -0300

    feat: Add search functionality to notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,50 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 interface Note {
-  id: string,
-  date: Date,
-  content: string
+  id: string;
+  date: Date;
+  content: string;
 }
 
 export function App() {
+  const [search, setSearch] = useState("");
+
   const [notes, setNotes] = useState<Note[]>(() => {
-  const notesOnStorage = localStorage.getItem('notes')
+    const notesOnStorage = localStorage.getItem("notes");
 
-  if(notesOnStorage) {
-    return JSON.parse(notesOnStorage)
-  }
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
 
-  return []
-
+    return [];
   });
 
   function OnNoteCreated(content: string) {
     const newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
-      content
-    }
+      content,
+    };
 
-    const notesArray = [newNote, ...notes]
+    const notesArray = [newNote, ...notes];
 
-    setNotes(notesArray)
+    setNotes(notesArray);
 
-    localStorage.setItem('notes', JSON.stringify(notesArray))
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes = search
+    ? notes.filter((note) => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes;
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -44,16 +55,17 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard onNoteCreated={OnNoteCreated}/>
+        <NewNoteCard onNoteCreated={OnNoteCreated} />
 
-        {notes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
         })}
       </div>
     </div>
